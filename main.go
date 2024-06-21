761296b2ca541a935a9f01e08c74c3fc26d2901e
Digkill <digkill@mail.ru>
2024-06-22 02:30:45 +0500

    fix env

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"github.com/tidwall/gjson"
 	"log"
 	"os"
+	"path/filepath"
 	"strconv"
 )
 
@@ -32,7 +33,12 @@ func New(dsn string) (*Storage, error) {
 }
 
 func main() {
-	err := godotenv.Load(".env")
+	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = godotenv.Load(filepath.Join(dir, ".env"))
 	if err != nil {
 		log.Fatalf("Some error occured. Err: %s", err)
 	}
